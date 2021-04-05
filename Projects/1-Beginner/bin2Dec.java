158e94c0d02fa72e8f56c80ee88e0ece8493ffa1
isaiasramalho <isaiasramalhoepai@gmail.com>
2021-04-04 22:53:41 -0300

    Begging to code bin2Dec.java

---
+++ Projects/1-Beginner/bin2Dec.java
@@ -0,0 +1,18 @@
+/**
+ * Programa para converter número binário informado pelo usuário em
+ * número decimal.
+ */
+
+public class bin2Dec
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Vamos convertar números binários em Decimal");
+    }
+    //
+    public int ConverteBin2Dec( String binario)
+    {
+        int decimal = 0;
+        return decimal;
+    }
+}
